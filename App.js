e099e3d3a558369b9ae3c61eb1282e6b56979d98
mdniyaz224 <68887332+mdniyaz224@users.noreply.github.com>
2021-08-13 21:53:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Menu from './Menu';
+import Ban from './Ban';
+import Card from './Card';
+import Footer from './Footer';
+
+
+
+
+
+function App() {
+  return (
+  <>
+  <div className="container-fluid bg-dark">
+  
+  <Menu></Menu>
+  <Ban></Ban>
+  <br/>
+  <hr/>
+  <br/>
+  <Card/>
+  <br/>
+  <Card/>
+  <br/>
+  <Card/>
+  <br/>
+  <hr/>
+  <br/>
+<Footer></Footer>
+</div>
+  </>
+  );
+}
+
+export default App;
