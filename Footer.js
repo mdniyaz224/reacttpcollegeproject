b473803935576fa7dbf6d8a2a159d960c850f12c
mdniyaz224 <68887332+mdniyaz224@users.noreply.github.com>
2021-08-13 21:50:09 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,16 @@
+import react from "react"
+const Footer = () => {
+   return (
+        <>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+      
+  <h1>hello</h1>
+        <h1>hello</h1>
+        </>
+    )
+}
+export default Footer
