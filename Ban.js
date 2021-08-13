f4aea70b2d0413d1e327d20d904367c9378a41c6
mdniyaz224 <68887332+mdniyaz224@users.noreply.github.com>
2021-08-13 21:48:39 +0530

    Add files via upload

---
+++ Ban.js
@@ -0,0 +1,13 @@
+import react from "react"
+import img1 from './image/house1.jpg'
+const Ban = () => {
+  return  (
+        <>
+    
+        <div className=  " ">
+        <img src={img1}  className="img-fluid" alt="my image"></img>
+        </div>
+        </>
+    )
+}
+export default Ban;
